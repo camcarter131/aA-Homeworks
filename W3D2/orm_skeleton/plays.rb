aba25c6c81c68d2d2f0870458e5feed3c91876c1
Cameron Carter <cameroncarter@utexas.edu>
2019-03-26 09:51:46 -0700

    Finish ORM hw

--- W3D2/orm_skeleton/plays.rb
+++ W3D2/orm_skeleton/plays.rb
@@ -28,6 +28,16 @@ class Play
     SQL
   end
 
+  def self.find_by_playwright(playwright)
+    PlayDBConnection.instance.execute(<<-SQL)
+      SELECT p.title
+      FROM plays as p
+      JOIN playwrights as pw
+      ON p.playwright_id = pw.id
+      WHERE pw.name = "#{playwright}"
+    SQL
+  end
+
   def initialize(options)
     @id = options['id']
     @title = options['title']
@@ -36,7 +46,7 @@ class Play
   end
 
   def create
-    raise "#{self} already in database" if self.id
+    raise "#{self} already in database" if !nil
     PlayDBConnection.instance.execute(<<-SQL, self.title, self.year, self.playwright_id)
       INSERT INTO
         plays (title, year, playwright_id)
@@ -57,4 +67,56 @@ class Play
         id = ?
     SQL
   end
+end
+
+class Playwright
+  attr_accessor :id, :name, :birth_year
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+    data.map { |datum| Playwright.new(datum) }
+  end
+
+  def self.find_by_name(name)
+    PlayDBConnection.instance.execute(<<-SQL)
+      SELECT * from playwrights
+      where name = "#{name}"
+    SQL
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
+
+  def create
+    raise "#{self} already in database" if self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year)
+      INSERT INTO
+        playwrights (name, birth_year)
+      VALUES
+        (?, ?)
+    SQL
+    self.id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year, self.id)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
+  end
+
+  def get_plays
+    PlayDBConnection.instance.execute(<<-SQL)
+      SELECT * from plays as p
+      where playwright_id = self.id
+    SQL
+  end
+
 end
